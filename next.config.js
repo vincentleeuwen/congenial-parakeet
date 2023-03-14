c7668cc092b4692fc1a74f6681f4fd5f8a1f7a14
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2023-03-14 16:58:38 +0100

    adjust timeout as some swapi endpoints can be quite slow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  staticPageGenerationTimeout: 60 * 5, // some swapi endpoints can be slow
 }
 
 module.exports = nextConfig
